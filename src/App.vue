46bd4a606bb72c7986cf249f964b0711dcac6eed
leeinhwan0421 <70061182+leeinhwan0421@users.noreply.github.com>
2026-01-12 18:49:08 +0900

    minor update [2026. 01. 12]
    - fixed design
    - fixed icon

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,18 @@
 </template>
 
 <script setup>
+import { watch } from 'vue';
+import { useI18n } from 'vue-i18n';
+
 import AppNavbar from '@/components/AppNavbar.vue';
 import AppFooter from '@/components/AppFooter.vue';
+
+const { t, locale } = useI18n();
+
+watch(
+        locale, 
+        (newLocale) => {document.title = t('title');}, 
+        { immediate: true }
+    );
 </script>
+
